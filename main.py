4962362019a7a75715877f7464ec991845db1a36
doding2 <dragon041414@gmail.com>
2024-12-07 19:56:47 +0900

    Update: Preprocess dataset more detail

--- main.py
+++ main.py
@@ -5,28 +5,48 @@ from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 import matplotlib.pyplot as plt
 from keras.src.models import Sequential
 from keras.src.layers import Dense, LSTM, Dropout
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+def preprocessing(dataset: pd.DataFrame) -> pd.DataFrame:
+    dataset = dataset[['가격', '신차대비가격', '최초등록일', '조회수', '배기량', '색상', '연비', '최고출력', '최대토크']]
+
+    # parse date
+    dataset['최초등록일'] = pd.to_datetime(dataset['최초등록일'], yearfirst=True, dayfirst=False)
+
+    # set index with 최초등록일
+    dataset = dataset.set_index('최초등록일')
+
+    # remove prefix and suffix from data
+    dataset['연비'] = dataset['연비'].apply(lambda x: str(x).replace("km/ℓ",""))
+    dataset['최고출력'] = dataset['최고출력'].apply(lambda x: str(x).replace("마력",""))
+    dataset['최대토크'] = dataset['최대토크'].apply(lambda x: str(x).replace("kg.m",""))
+    dataset['연비'] = pd.to_numeric(dataset['연비'], errors='coerce')
+    dataset['최고출력'] = pd.to_numeric(dataset['최고출력'], errors='coerce')
+    dataset['최대토크'] = pd.to_numeric(dataset['최대토크'], errors='coerce')
+
+    # encode categorical data to numeric data
+    encoder = LabelEncoder()
+    dataset['색상'] = encoder.fit_transform(dataset['색상'])
+
+    # min-max scaling and sort by date
+    print(dataset.isna().sum())
+    dataset = dataset.dropna(axis=0, how='any')
+    dataset = dataset.sort_index()
+    dataset = pd.DataFrame(MinMaxScaler().fit_transform(dataset), columns=dataset.columns, index=dataset.index)
+    print(dataset)
+
+    return dataset
 
 
 def main():
     # read dataset
     dataset = pd.read_csv('dataset/genesis_large.csv')
 
-    # # remove outliers
-    # dataset = dataset[dataset['가격'] < 9999]
+    # preprocessing
+    dataset = preprocessing(dataset)
 
-    # set index with 최초등록일
-    dataset['최초등록일'] = pd.to_datetime(dataset['최초등록일'], yearfirst=True, dayfirst=False)
-    dataset = dataset.set_index('최초등록일')
-    print(dataset.describe())
-
-    # min-max scaling and sort by date
-    dataset = dataset[['가격', '신차대비가격', '신차가격']]
-    dataset = dataset.dropna()
-    dataset = dataset.sort_index()
-    dataset = pd.DataFrame(MinMaxScaler().fit_transform(dataset), columns=dataset.columns, index=dataset.index)
-    print(dataset)
-
-    dataset_X = dataset[['신차대비가격', '신차가격']].values.tolist()
+    dataset_X = dataset[['신차대비가격', '조회수', '배기량', '색상', '연비', '최고출력', '최대토크']].values.tolist()
     dataset_y = dataset[['가격']].values.tolist()
 
     # group 30 data into a single window
@@ -56,7 +76,7 @@ def main():
 
     # prepare hyperparameters of model
     model = Sequential()
-    model.add(LSTM(units=20, activation='relu', return_sequences=True, input_shape=(window_size, 2)))
+    model.add(LSTM(units=20, activation='relu', return_sequences=True, input_shape=(window_size, 7)))
     model.add(Dropout(0.1))
     model.add(LSTM(units=20, activation='relu'))
     model.add(Dropout(0.1))
